0c3d4719faaeb1c5055cdd1b004c3ff30fb5d837
Dimitrius <speed.Dima05@gmail.com>
2025-11-19 23:49:43 +0300

    Add comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
+// This program prints hello message with user's name
 int main() {
-    std::string name;
+    std::string name; // user name
     std::cin >> name;
     std::cout << "Hello world from " << name;
 }
